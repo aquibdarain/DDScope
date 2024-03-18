097dcbc7bb9ab1aa0ff2733b89ba25e7f8ed5d21
Abhay2546 <abhayzingreb@gmail.com>
2024-03-18 10:41:51 +0530

    updated vision.php

--- application/views/vision.php
+++ application/views/vision.php
@@ -1,156 +1,180 @@
-<?php include("application/views/header_view.php");?>
+<?php include("application/views/header_view.php"); ?>
 
-    <section class="innerbanner ">
-      <div class="container">
+<!-- <section class="innerbanner ">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>Our Vision</h1>
+      </div>
+      <div class="col-lg-6 mb-2">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?php echo base_url(''); ?>">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">
+              Our Vision
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <br><br>
+    </div>
+  </div>
+</section> -->
+<section class="innerbanner ">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>Our Vision</h1>
+      </div>
+      <div class="col-lg-6 mb-2">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?php echo base_url(''); ?>">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">
+              Our Vision
+            </li>
+          </ol>
+          <!-- <br> -->
+        </nav>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="main-content">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Awesome Features</h2>
+      </div>
+      <div class="col-lg-3 text-center">
+        <div class="feature">
+          <div class="feature-icon"><i class="bi bi-gem"></i></div>
+          <p>Proven and Innovative Trading Strategy</p>
+        </div>
+      </div>
+      <div class="col-lg-3 text-center">
+        <div class="feature">
+          <div class="feature-icon"><i class="bi bi-pin-map-fill"></i></div>
+          <p>
+            A Stock search engine based on performance metrics relevant to
+            stressless trading method
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-3 text-center">
+        <div class="feature">
+          <div class="feature-icon"><i class="bi bi-heart-fill"></i></div>
+          <p>
+            A price-ladder based interface for effectively selecting the
+            stressless trading method parameters
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-3 text-center">
+        <div class="feature">
+          <div class="feature-icon">
+            <i class="bi bi-battery-charging"></i>
+          </div>
+          <p>
+            A cash management system that automatically allocates cash for
+            withdrawal or reinvestment as the trading game proceeds
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="bg-con">
+  <div class="container">
+    <div class="row mt-5 mb-5">
+      <div class="col-lg-12">
+        <h2>Our Solutions</h2>
+      </div>
+      <div class="col-lg-12">
         <div class="row">
-          <div class="col-lg-12">
-            <h1>Our Vision</h1>
+          <div class="col-lg-6 text-center">
+            <img src="<?php echo base_url('images/solutionbg.png'); ?>" />
           </div>
           <div class="col-lg-6">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?php echo base_url(''); ?>">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">
-                  Our Vision
-                </li>
-              </ol>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="main-content">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <h2>Awesome Features</h2>
-          </div>
-          <div class="col-lg-3 text-center">
-            <div class="feature">
-              <div class="feature-icon"><i class="bi bi-gem"></i></div>
-              <p>Proven and Innovative Trading Strategy</p>
-            </div>
-          </div>
-          <div class="col-lg-3 text-center">
-            <div class="feature">
-              <div class="feature-icon"><i class="bi bi-pin-map-fill"></i></div>
+            <div class="d-block cardbox">
+              <h3>Intuitive</h3>
               <p>
-                A Stock search engine based on performance metrics relevant to
-                stressless trading method
+                An intuitive trading strategy that ensures an executable
+                plan in every market condition
               </p>
             </div>
-          </div>
-          <div class="col-lg-3 text-center">
-            <div class="feature">
-              <div class="feature-icon"><i class="bi bi-heart-fill"></i></div>
+            <div class="d-block cardbox">
+              <h3>Stressless</h3>
               <p>
-                A price-ladder based interface for effectively selecting the
-                stressless trading method parameters
+                A Stressless trading system that is as easy to manage as
+                placing a trade
               </p>
             </div>
-          </div>
-          <div class="col-lg-3 text-center">
-            <div class="feature">
-              <div class="feature-icon">
-                <i class="bi bi-battery-charging"></i>
-              </div>
+            <div class="d-block cardbox">
+              <h3>WOW</h3>
               <p>
-                A cash management system that automatically allocates cash for
-                withdrawal or reinvestment as the trading game proceeds
+                A cash management System that provides regular cash
+                extraction amount proportional to the stock price variance
               </p>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <section class="bg-con">
-      <div class="container">
         <div class="row mt-5 mb-5">
           <div class="col-lg-12">
-            <h2>Our Solutions</h2>
+            <h2>Value Proposal</h2>
           </div>
-          <div class="col-lg-12">
-            <div class="row">
-              <div class="col-lg-6 text-center">
-                <img src="<?php echo base_url('images/solutionbg.png');?>" />
-              </div>
-              <div class="col-lg-6">
-                <div class="d-block cardbox">
-                  <h3>Intuitive</h3>
-                  <p>
-                    An intuitive trading strategy that ensures an executable
-                    plan in every market condition
-                  </p>
-                </div>
-                <div class="d-block cardbox">
-                  <h3>Stressless</h3>
-                  <p>
-                    A Stressless trading system that is as easy to manage as
-                    placing a trade
-                  </p>
-                </div>
-                <div class="d-block cardbox">
-                  <h3>WOW</h3>
-                  <p>
-                    A cash management System that provides regular cash
-                    extraction amount proportional to the stock price variance
-                  </p>
-                </div>
-              </div>
+          <div class="col-lg-6">
+            <div class="d-block cardbox">
+              <h3>Make things really change</h3>
+              <p>
+                Make automated trading a household concept for retail traders
+              </p>
             </div>
-            <div class="row mt-5 mb-5">
-              <div class="col-lg-12">
-                <h2>Value Proposal</h2>
-              </div>
-              <div class="col-lg-6">
-                <div class="d-block cardbox">
-                  <h3>Make things really change</h3>
-                  <p>
-                    Make automated trading a household concept for retail traders
-                  </p>
-                </div>
-                <div class="d-block cardbox">
-                  <h3>Make the world a better place</h3>
-                  <p>
-                    Removes Stress from trading for retailers, and generate cash
-                    automatically for periodic withdrawal
-                  </p>
-                </div>
-                <div class="d-block cardbox">
-                  <h3>Offer one perfect answer</h3>
-                  <p>
-                    Set the stressless trading method as the foundation for more
-                    complex predictive algorithms
-                  </p>
-                </div>
-              </div>
-              <div class="col-lg-6 text-center">
-                <img src="<?php echo base_url('images/praposalbg.png');?>" />
-              </div>
+            <div class="d-block cardbox">
+              <h3>Make the world a better place</h3>
+              <p>
+                Removes Stress from trading for retailers, and generate cash
+                automatically for periodic withdrawal
+              </p>
             </div>
+            <div class="d-block cardbox">
+              <h3>Offer one perfect answer</h3>
+              <p>
+                Set the stressless trading method as the foundation for more
+                complex predictive algorithms
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-6 text-center">
+            <img src="<?php echo base_url('images/praposalbg.png'); ?>" />
           </div>
         </div>
       </div>
-    </section>
-    <footer class="footer-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <p>
-                    <a href="<?php echo base_url('');?>">Home</a> |
-                    <a href="<?php echo base_url('Home/vision');?>">Our vision</a> |
-                    <a href="<?php echo base_url('Home/career');?>">Careers</a> |
-                    <a href="<?php echo base_url('Home/video');?>">Videos</a> |
-                    <a href="<?php echo base_url('Home/ads');?>">Ads</a>|
-                    <a href="<?php echo base_url('Home/policy');?>">Privacy Policy </a>
-                </p>
-                <p>contact@dozendiamonds.com</p>
-                <p>Copyright © 2023 DozenDiamonds. All Rights Reserved.</p>
-            </div>
-        </div>
     </div>
+  </div>
+</section>
+
+
+<footer class="footer-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <p>
+          <a href="<?php echo base_url(''); ?>">Home</a> |
+          <a href="<?php echo base_url('Home/vision'); ?>">Our vision</a> |
+          <a href="<?php echo base_url('Home/career'); ?>">Careers</a> |
+          <a href="<?php echo base_url('Home/video'); ?>">Videos</a> |
+          <a href="<?php echo base_url('Home/ads'); ?>">Ads</a>|
+          <a href="<?php echo base_url('Home/policy'); ?>">Privacy Policy </a>
+        </p>
+        <p>contact@dozendiamonds.com</p>
+        <p>Copyright © 2023 DozenDiamonds. All Rights Reserved.</p>
+      </div>
+    </div>
+  </div>
 </footer>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <!--     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>-->
-  </body>
+<script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+<!--     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>-->
+</body>
+
 </html>
