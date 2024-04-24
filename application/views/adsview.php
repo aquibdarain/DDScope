ebc29b03e9580324f6cc4cd6b9c5c7c03172ab67
Abhay2546 <abhayzingreb@gmail.com>
2024-04-24 11:08:42 +0530

    updation ads.php code

--- application/views/adsview.php
+++ application/views/adsview.php
@@ -1,5 +1,5 @@
 <?php include("application/views/header_view.php"); ?>
- 
+
 <section class="innerbanner">
     <div class="container">
         <div class="row">
@@ -16,6 +16,7 @@
             <?php
             // YouTube video URLs
             $video_urls = [
+                "https://www.youtube.com/embed/90W9p1tkzZk",
                 "https://www.youtube.com/embed/Pu2QwQVbmo8",
                 "https://www.youtube.com/embed/EZvCpjfWRPI",
                 "https://www.youtube.com/embed/Tyr9Ba9JEpY",
@@ -33,13 +34,15 @@
                 "https://www.youtube.com/embed/pNh5aCNfhCg",
                 "https://www.youtube.com/embed/QzqkbO4aJnI",
                 "https://www.youtube.com/embed/vQFg0QqoCh4",
-                "https://www.youtube.com/embed/tSVRLyoFkBc"
-
-
+                "https://www.youtube.com/embed/tSVRLyoFkBc",
+                "https://www.youtube.com/embed/MoxMva7mtvk",
+                "https://www.youtube.com/embed/yWVPwmlaM1I",
+                "https://youtube.com/embed/NzPzov3xNAA"
             ];
 
             // Static captions for the videos
             $captions = [
+                "Stressless Trading Method and Extra Cash on a Spreadsheet.",
                 "Don’t mix lots purchased at different times for the same stock.",
                 "Track profits and losses for each purchased lot of a given stock.",
                 "Make sure you never run out of cash no matter how low the market goes.",
@@ -55,27 +58,21 @@
                 "Understand the market mechanics of buying and selling shares.",
                 "Don’t overburden yourself by stressing about your portfolio.",
                 "Sources of stress in stock trading.",
-                "An application designed for automatic trading." ,
+                "An application designed for automatic trading.",
                 "Dozen Diamonds offers systematic purchase lot management.",
-                "Suminderpal singh testimonial for Dozen Diamonds."
+                "Suminderpal singh testimonial for Dozen Diamonds.",
+                "DD ads unpredictable markets and extra cash.",
+                "DD shorts 2 Lending money to a friend 16 9 ratio",
+                "Dozen Diamonds introduction"
             ];
-            
+
 
             // Loop through each video URL
             for ($i = 0; $i < count($video_urls); $i++) {
             ?>
                 <div class="col mb-4">
                     <div class="card-box shadow rounded">
-                        <iframe
-                            width="100%"
-                            height="200"
-                            class="video"
-                            src="<?php echo $video_urls[$i]; ?>"
-                            title="YouTube video player"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowfullscreen
-                        ></iframe>
+                        <iframe width="100%" height="200" class="video" src="<?php echo $video_urls[$i]; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         <p class="caption"><?php echo $captions[$i]; ?></p> <!-- Static caption for the video -->
                     </div>
                 </div>
@@ -87,22 +84,22 @@
 </section>
 
 <?php
- $num_br_tags = 2;
- for ($i = 0; $i < $num_br_tags; $i++) {
+$num_br_tags = 2;
+for ($i = 0; $i < $num_br_tags; $i++) {
     echo "<br>";
 }
 ?>
-    <footer class="footer-section">
+<footer class="footer-section">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <p>
-                    <a href="<?php echo base_url('');?>">Home</a> |
-                    <a href="<?php echo base_url('Home/vision');?>">Our vision</a> |
-                    <a href="<?php echo base_url('Home/career');?>">Careers</a> |
-                    <a href="<?php echo base_url('Home/video');?>">Videos</a> |
-                    <a href="<?php echo base_url('Home/ads');?>">Ads</a>|
-                    <a href="<?php echo base_url('Home/policy');?>">Privacy Policy </a>
+                    <a href="<?php echo base_url(''); ?>">Home</a> |
+                    <a href="<?php echo base_url('Home/vision'); ?>">Our vision</a> |
+                    <a href="<?php echo base_url('Home/career'); ?>">Careers</a> |
+                    <a href="<?php echo base_url('Home/video'); ?>">Videos</a> |
+                    <a href="<?php echo base_url('Home/ads'); ?>">Ads</a>|
+                    <a href="<?php echo base_url('Home/policy'); ?>">Privacy Policy </a>
                 </p>
                 <p>contact@dozendiamonds.com</p>
                 <p>Copyright © 2023 DozenDiamonds. All Rights Reserved.</p>
@@ -135,4 +132,5 @@
 <script src="js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript" src="jquery-3.4.1.min.js"></script>
 </body>
+
 </html>
